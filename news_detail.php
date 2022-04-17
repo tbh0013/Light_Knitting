1c444d9e0c22f3c67f8bcb430b289e2e3053cfce
tbh0013 <tbh0013@gmail.com>
2022-04-17 13:20:36 +0900

    fix: sql文修正

--- news_detail.php
+++ news_detail.php
@@ -9,7 +9,7 @@ if (!is_numeric($news_id)) {
     exit();
 }
 
-$news_st = $pdo->query("SELECT to_char(date,'YYYY-MM-DD') as t_date,, news_id,title, text, url, image_path, product_id, is_deleted FROM news WHERE news_id = $news_id AND is_deleted = 0");
+$news_st = $pdo->query("SELECT to_char(date,'YYYY-MM-DD') as t_date, news_id,title, text, url, image_path, product_id, is_deleted FROM news WHERE news_id = $news_id AND is_deleted = 0");
 $news_st->setFetchMode(PDO::FETCH_ASSOC);
 $newslist = $news_st->fetchAll();
 
